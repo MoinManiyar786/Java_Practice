c08458ad68130938f9b1bfd814d9d31b434e75eb
Shubham Tade <shubham.tade@gmail.com>
2024-10-21 03:40:12 +0530

    java programs

--- Forl.java
+++ Forl.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class Forl {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for (int num = 1;num<=n;num++){
+            System.out.println(num);
+        }
+    }
 }
--- HEl.java
+++ HEl.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class HEl {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter value only shows the first word: ");
+    String name = input.next();
+    System.out.println("Enter value only shows the first word: ");
+    String name1 = input.nextLine();
+    System.out.println("The word is "+name);
+        System.out.println("the full sentence is "+name1);
+    input.close();
+    }
 }
