87952024e5b0c48c38de86cf1f995714fce23052
Moin Maniyar <124362454+MoinManiyar786@users.noreply.github.com>
2024-10-21 02:31:26 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 
 public class Add{
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int a = input.nextInt();
-        int b = input.nextInt();
-        int c = a + b;
+        Scanner input = new Scanner(System.in
+        int a,b,c;
+        a = input.nextInt();
+        b = input.nextInt();
+        c = a + b;
         System.out.println("The addition is " + c);
         input.close();
     }
