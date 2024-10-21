2e77a405f6fbe0301fd4bc2bd30888c92081cbcb
Shubham Tade <shubham.tade@gmail.com>
2024-10-22 02:47:00 +0530

    Delete

--- Oeven.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Oeven {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        char abs = input.next().charAt(0);
-        System.out.println("the char is " + abs);
-        input.close();
-    }
-}
