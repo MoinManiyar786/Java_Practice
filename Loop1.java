d62a8f46b5ff31ccf6af3c21276f1e2efbd5f11b
MoinManiyar <moinraza313786@gmail.com>
2024-11-03 22:18:03 +0530

    loops

---
+++ Loop1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Loop1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("here the all alphabets are shown");
+        char i = 'a';
+        while(i<='z'){
+            System.out.println(i + " ");
+            i++;
+        }
+
+    }
+}
