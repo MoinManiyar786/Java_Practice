8facd70da84250e4c231c40b156df86be36798e7
MoinManiyar <moinraza313786@gmail.com>
2024-11-06 20:25:21 +0530

    new

---
+++ HL.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class HL {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num = input.nextInt();
+        for (int i=1;i<=num;i++) {
+            System.out.println("Hello world");
+        }
+    }
+}
