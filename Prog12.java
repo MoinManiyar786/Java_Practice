1801494c97e095e664a723ca9eed83808da707ac
MoinManiyar <moinraza313786@gmail.com>
2024-11-09 17:34:53 +0530

    new

---
+++ Prog12.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Prog12 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("ASCII values from a-z");
+        for (int i = 'a' ;i<='z' ;i++){
+            System.out.println(i);
+        }
+    }
+}
